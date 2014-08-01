655910afc52daaa945a9921c970f0de9e0e41a61
Jeremiah Lant <jlant@usgs.gov>
2014-08-01 12:18:52 -0400

    added new wateruse_factors.txt and changed variables

--- waterapputils/wateruse_batch_variables.py
+++ waterapputils/wateruse_batch_variables.py
@@ -7,19 +7,25 @@ waterbatch_directory = "C:/Users/jlant/jeremiah/temp/water-delaware-river-basin/
 output_directory = "C:/Users/jlant/jeremiah/temp/water-delaware-river-basin/2014-08-01_testing/wateruse/output/"
 
 # list of paths to water use files to use
-wateruse_files = ["../data/wateruse-datafiles/test_JFM.txt", 
-                  "../data/wateruse-datafiles/test_AMJ.txt", 
-                  "../data/wateruse-datafiles/test_JAS.txt", 
-                  "../data/wateruse-datafiles/test_OND.txt"] 
+wateruse_files = ["../data/wateruse-datafiles/S1_JFM_WU_2014jul7.txt", 
+                  "../data/wateruse-datafiles/S2_AMJ_WU_2014jul7.txt", 
+                  "../data/wateruse-datafiles/S3_JAS_WU_2014jul7.txt", 
+                  "../data/wateruse-datafiles/S4_OND_WU_2014jul7.txt"] 
+
+#wateruse_files = ["../data/wateruse-datafiles/test_JFM.txt", 
+#                  "../data/wateruse-datafiles/test_AMJ.txt", 
+#                  "../data/wateruse-datafiles/test_JAS.txt", 
+#                  "../data/wateruse-datafiles/test_OND.txt"] 
                   
 # path to the water use factor file                 
-wateruse_factor_file =  "../data/wateruse-datafiles/test_wateruse_factors.txt"
+wateruse_factor_file =  "../data/wateruse-datafiles/wateruse_factors.txt"
 
 # path to the basin centroids shapefile
-basin_centroids_shapefile = "../data/spatial-datafiles/basins/dem_basin_centroids_proj_wgs.shp"
+basin_centroids_shapefile = "../data/spatial-datafiles/basins/dem_basin_centroids.shp"
 
 # path to the basin shapefile used in the WATER batch run
-basin_shapefile = "../data/spatial-datafiles/basins/waterbasin_multi_clean_proj_wgs.shp"
+#basin_shapefile = "C:/Users/jlant/jeremiah/temp/water-delaware-river-basin/2014-06-06_testbatch_clean/Watersheds.shp"
+basin_shapefile = "../data/spatial-datafiles/basins/waterbasin_multi_clean.shp"
 
 # field in the basin shapefile used in the WATER batch run to link to water use files
 basin_field = "STAID"
