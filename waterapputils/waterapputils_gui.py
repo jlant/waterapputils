752de153f3bae79c795249779a33e7a0335c53ec
Jeremiah Lant <jlant@usgs.gov>
2015-02-05 10:09:33 -0500

    added an about method; added method to open a file dialog and select a WATER.txt file.

--- waterapputils/waterapputils_gui.py
+++ waterapputils/waterapputils_gui.py
@@ -4,10 +4,12 @@ from gui.user_interface import Ui_MainWindow
 
 # my modules
 class MainWindow(QtGui.QMainWindow):
-	""" """
+	""" Subclass of QMainWindow that creates the main window """
+
 	def __init__(self, parent = None):
+
 		super(MainWindow, self).__init__(parent)
-		self.setWindowTitle("My Simulation")
+		self.setWindowTitle("WATERAPPUTILS GUI")
 
 		# set up the ui
 		self.ui = Ui_MainWindow()
@@ -15,6 +17,33 @@ class MainWindow(QtGui.QMainWindow):
 
 		# connections
 		self.ui.actionExit.triggered.connect(self.close)
+		self.ui.actionAbout.triggered.connect(self.about)
+		self.ui.push_button_open_file.clicked.connect(self.process_watertxt_file)
+
+	def about(self):
+		""" Show an message box about the gui."""
+		
+		msg = \
+		"""
+		The waterapputils gui can be used to process and interact with output
+		and database files from the WATER application.  More help and information
+		can be found at https://github.com/jlant-usgs/waterapputils
+		"""
+
+		QtGui.QMessageBox.about(self, "About the waterapputils gui", msg.strip())
+
+	def _select_file(self):
+		""" Open a QtDialog to select a WATER.txt file and show the file in the line edit widget """
+
+		filepath = QtGui.QFileDialog.getOpenFileName(self, caption="Please select a WATER.txt file", directory="../data/watertxt-datafiles/", filter="Text files (*.txt);; All files (*.*)")
+		if filepath:
+			self.ui.line_edit_open_file.setText(filepath)
+
+	def process_watertxt_file(self):
+		""" Open a file dialog to select, process, and plot a WATER.txt file."""
+
+		self._select_file()
+
 
 def main():
 	""" """
