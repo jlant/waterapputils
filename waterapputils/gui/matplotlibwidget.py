ace07c0e16475f185cd05dffe598605e24aa6035
Jeremiah Lant <jlant@usgs.gov>
2015-02-05 11:15:10 -0500

    added a matplotlibwidget to plot a parameter in a WATER.txt file

--- waterapputils/gui/matplotlibwidget.py
+++ waterapputils/gui/matplotlibwidget.py
@@ -1,26 +1,44 @@
-import random
-
 from PyQt4 import QtGui
 from PyQt4 import QtCore
 
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
-
 from matplotlib.figure import Figure
-
 from matplotlib.widgets import SpanSelector
-
 import numpy as np
+
+# Global colors dictionary
+COLORS = {"Discharge": "b",
+          "Subsurface Flow": "g",
+          "Impervious Flow": "SteelBlue",
+          "Infiltration Excess": "SeaGreen",
+          "Initial Abstracted Flow": "MediumBlue",
+          "Overland Flow": "RoyalBlue",
+          "PET": "orange",
+          "AET": "DarkOrange",
+          "Average Soil Root zone": "Gray",
+          "Average Soil Unsaturated Zone": "DarkGray",
+          "Snow Pack": "PowderBlue",
+          "Precipitation": "SkyBlue",
+          "Storage Deficit": "Brown",
+          "Return Flow": "Aqua",
+          "Water Use": "DarkCyan",
+          "Discharge + Water Use": "DarkBlue"
+}
 
 class MatplotlibWidget(QtGui.QWidget):
     """ This subclass of QtWidget will manage the widget drawing; name matches the class in the *_ui.py file"""    
     
     def __init__(self, parent = None):
         super(MatplotlibWidget, self).__init__(parent)
-        
+        super(MatplotlibWidget, self).__init__(parent)
+
+        # create initial values for watertxt data
+        self.parameter = None
+
         # create figure
         self.figure = Figure()
-        
+
         # create canvas and set some of its properties
         self.canvas = FigureCanvas(self.figure)
         self.canvas.setParent(parent) 
@@ -28,64 +46,117 @@ class MatplotlibWidget(QtGui.QWidget):
         self.canvas.updateGeometry()
         self.canvas.setFocusPolicy(QtCore.Qt.StrongFocus)
         self.canvas.setFocus()
-        
+
         # set up axes and its properties
         self.axes1 = self.figure.add_subplot(211) 
         self.axes2 = self.figure.add_subplot(212)
         #self.axes2 = self.figure.add_subplot(212, sharex = self.axes1, sharey = self.axes1)
-        
+
         self.axes1.grid(True)
         self.axes2.grid(True)
-        
+
         # create widgets  
         self.matplotlib_toolbar = NavigationToolbar(self.canvas, parent) # the matplotlib toolbar object
-        
+
         # create the layout
         self.layout = QtGui.QVBoxLayout()
-        
+
         # add the widgets to the layout
-        self.layout.addWidget(self.canvas)
         self.layout.addWidget(self.matplotlib_toolbar)
-        
+        self.layout.addWidget(self.canvas)
+
         # set the layout
         self.setLayout(self.layout)
 
         # create SpanSelector        
-        self.span = SpanSelector(self.axes1, self.on_select, 'horizontal', useblit=True,
-                                 rectprops=dict(alpha=0.5, facecolor='red') )
-        
-    def plot_rand(self, axes):
-        """ method to plot random data """
-        random_numbers = random.sample(range(0, 10), 10)
-        axes.clear()
-        axes.set_title("Random Numbers")
-        axes.plot(random_numbers, '*-')
-        self.canvas.draw()
+        self.span = SpanSelector(self.axes1, self.on_select, 'horizontal', useblit=True, rectprops=dict(alpha=0.5, facecolor='red'))
 
-    def plot(self, x, y):
-        """ method to plot data"""
-        self.x = x
-        self.y = y
-        self.axes1.clear()
-        self.axes1.grid(True)
-        self.axes1.set_title("Simple x vs y Plot")
-        self.axes1.set_xlabel("x")
-        self.axes1.set_ylabel("y")
-        self.axes1.plot(self.x, self.y, 'b-*')
-        self.axes2.plot(self.x, self.y, 'r-.')
-        self.canvas.draw()
-        
+       
     def on_select(self, xmin, xmax):
-        indmin, indmax = np.searchsorted(self.x, (xmin, xmax))
-        indmax = min(len(self.x) - 1, indmax)
-    
-        thisx = self.x[indmin:indmax]
-        thisy = self.y[indmin:indmax]
-        print(thisx)
-        print(thisy)
+        """ A select handler for SpanSelector that updates axes 2 with the new x and y limits selected by user """
+
+        # convert matplotlib float dates to a datetime format
+        date_min = mdates.num2date(xmin)
+        date_max = mdates.num2date(xmax) 
+
+        # put the xmin and xmax in datetime format to compare
+        date_min = datetime.datetime(date_min.year, date_min.month, date_min.day, date_min.hour, date_min.minute)    
+        date_max = datetime.datetime(date_max.year, date_max.month, date_max.day, date_max.hour, date_max.minute)
+
+        # find the indices that were selected    
+        indices = np.where((self.dates >= date_min) & (self.dates <= date_max))
+        indices = indices[0]
+           
+        thisx = self.dates[indices]
+        thisy = self.parameter["data"][indices]
+
         self.axes2.plot(thisx, thisy, 'r-.')
         self.axes2.set_xlim(thisx[0], thisx[-1])
         self.axes2.set_ylim(thisy.min(), thisy.max())
-        self.canvas.draw()      
+        self.canvas.draw() 
 
-    
+    def toggle_selector(self, event):
+        """ 
+        A toggle key event handler for the matplotlib SpanSelector widget.
+        A or a actives the slider; Q or q de-activates the slider.
+        """ 
+        if event.key in ['Q', 'q'] and self.span.visible:
+            print '**SpanSelector deactivated.**'
+            self.span.visible = False
+        if event.key in ['A', 'a'] and not self.span.visible:
+            print '**SpanSelector activated.**'
+            self.span.visible = True
+
+    def clearplot(self):
+        """ Clear the plot axes """ 
+        self.axes1.clear()
+        self.axes2.clear()
+        self.axes1.grid(True)
+        self.axes2.grid(True)
+
+    def plot_watertxt_parameter(self, watertxt_data, name): 
+        """ Plot a parameter from a WATER.txt file """
+
+        self.clearplot()
+
+        self.parameter = watertxt.get_parameter(watertxt_data, name = name)     
+
+        assert parameter is not None, "Parameter name {} is not in watertxt_data".format(name)
+
+        dates = watertxt_data["dates"]
+        self.dates = watertxt_data["dates"]
+        self.axes1.set_title("Parameter: {}".format(parameter["name"]))
+        self.axes1.set_xlabel("Date")
+        ylabel = "\n".join(wrap(parameter["name"], 60))
+        self.axes1.set_ylabel(ylabel)
+
+        # get proper color that corresponds to parameter name
+        color_str = COLORS[name.split('(')[0].strip()]
+
+        # plot parameter    
+        self.axes1.plot(dates, parameter["data"], color = color_str, label = parameter["name"], linewidth = 2)   
+
+        self.axes2.plot(dates, parameter["data"], color = "red", label = parameter["name"], linewidth = 2)
+
+        # rotate and align the tick labels so they look better
+        self.figure.autofmt_xdate()
+
+        # use a more precise date string for the x axis locations in the
+        # toolbar
+        self.axes1.fmt_xdata = mdates.DateFormatter("%Y-%m-%d")
+
+        # legend; make it transparent    
+        handles, labels = self.axes1.get_legend_handles_labels()
+        legend = self.axes1.legend(handles, labels, fancybox = True)
+        legend.get_frame().set_alpha(0.5)
+        legend.draggable(state=True)
+
+        # show text of mean, max, min values on graph; use matplotlib.patch.Patch properies and bbox
+        text = "mean = %.2f\nmax = %.2f\nmin = %.2f" % (parameter["mean"], parameter["max"], parameter["min"])
+        patch_properties = {"boxstyle": "round", "facecolor": "wheat", "alpha": 0.5}
+                       
+        self.axes1.text(0.05, 0.95, text, transform = self.axes1.transAxes, fontsize = 14, 
+                verticalalignment = "top", horizontalalignment = "left", bbox = patch_properties)    
+
+        self.canvas.draw()
+
--- waterapputils/waterapputils_gui.py
+++ waterapputils/waterapputils_gui.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt4 import QtGui, QtCore
 from gui.user_interface import Ui_MainWindow
+import modules.watertxt as watertxt
 
 # my modules
 class MainWindow(QtGui.QMainWindow):
@@ -10,6 +11,9 @@ class MainWindow(QtGui.QMainWindow):
 
 		super(MainWindow, self).__init__(parent)
 		self.setWindowTitle("WATERAPPUTILS GUI")
+
+		# initialize variables
+		self.watertxt_data = None
 
 		# set up the ui
 		self.ui = Ui_MainWindow()
@@ -32,21 +36,36 @@ class MainWindow(QtGui.QMainWindow):
 
 		QtGui.QMessageBox.about(self, "About the waterapputils gui", msg.strip())
 
-	def _select_file(self):
+	def _select_watertxt_file(self):
 		""" Open a QtDialog to select a WATER.txt file and show the file in the line edit widget """
 
-		filepath = QtGui.QFileDialog.getOpenFileName(self, caption="Please select a WATER.txt file", directory="../data/watertxt-datafiles/", filter="Text files (*.txt);; All files (*.*)")
+		filepath = QtGui.QFileDialog.getOpenFileName(self, caption = "Please select a WATER.txt file", directory = "../data/watertxt-datafiles/", filter = "Text files (*.txt);; All files (*.*)")
 		if filepath:
 			self.ui.line_edit_open_file.setText(filepath)
 
-	def process_watertxt_file(self):
-		""" Open a file dialog to select, process, and plot a WATER.txt file."""
+	def _read_watertxt_file(self):
+		""" Read a WATER.txt file """
+		self.watertxt_data = watertxt.read_file(filepath = self.ui.line_edit_open_file.text())
 
-		self._select_file()
+	def _add_to_list_widget(self):
+		""" Add column_names to list widget """
+		self.ui.list_widget.addItems(self.watertxt_data["column_names"])
+
+	def _plot_watertxt_file(self):
+		""" Plot a WATER.txt file """ 
+		self.ui.matplotlib_widget.plot_watertxt_parameter(watertxt_data = self.watertxt_data, name = self.watertxt_data["column_names"][0])
+
+	def process_watertxt_file(self):
+		""" Open a file dialog to select, read, display column names, and plot a WATER.txt file."""
+
+		self._select_watertxt_file()
+		self._read_watertxt_file()
+		self._add_to_list_widget()
+		# self._plot_watertxt_file()
 
 
 def main():
-	""" """
+	""" Run application """
 	app = QtGui.QApplication(sys.argv)		# create application object
 	main_window = MainWindow()				# create main window object
 	main_window.show()			
