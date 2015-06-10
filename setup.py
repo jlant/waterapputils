aaae9eb432d7d1b28e7ddf803859695ee08ffccc
Jeremiah Lant <jlant@usgs.gov>
2015-06-10 09:39:17 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,20 @@ try:
 except ImportError:
 	from distutils.core import setup
 
-with open('README.rst') as f:    
+with open('README.md') as f:    
 	readme = f.read()
 
 with open('LICENSE.txt') as f:    
 	license = f.read()	
 	
 setup(
-	name = 'nwispy',
+	name = 'waterapputils',
 	version = '1.0.0',
-	description = 'Read, process, print, and plot hydrologic data from USGS NWIS data files.',
+	description = 'Process simulations from a hydrologic model and apply water use and global circulation model deltas to model simulations.',
 	long_description = readme,
 	author = 'Jeremiah Lant',
 	author_email = 'jlant@usgs.gov',
-	url = 'https://github.com/jlant-usgs/nwispy',
+	url = 'https://github.com/jlant-usgs/waterapputils',
 	license = license,
 	packages = find_packages()
 	)
